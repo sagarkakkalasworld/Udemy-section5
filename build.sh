e67654e73980949c7beb17e5607bb8b7af0483a6
sagar <sagar@sagars-Air.lan>
2025-05-15 13:57:37 +0530

    added dockerfile

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/Udemy-section5/
+git pull
+npm install
+npm run build
---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/ubuntu/Udemy-section5
+git pull
+chmod 744 push_docker_image.sh
+/home/ubuntu/Udemy-section4-code/push_docker_image.sh
+microk8s kubectl rollout restart deployment react-deployment -n react-microk8s
