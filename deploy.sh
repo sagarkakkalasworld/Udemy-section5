515d276bd91c361a9512b83352c1e0dbcfed0908
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-05-29 16:41:50 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/Udemy-section5
-chmod 744 push_docker_image.sh
-/home/ubuntu/Udemy-section5/push_docker_image.sh
 microk8s kubectl rollout restart deployment react-deployment -n react-microk8s
